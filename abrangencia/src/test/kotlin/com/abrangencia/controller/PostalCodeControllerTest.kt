2718c2a3925f18f91ba0e9b1ce32a4896a19b4b5
leandro-victorf <leandro.victorf@gmail.com>
2021-12-23 17:50:58 -0300

    Fix test getPostalCode

--- abrangencia/src/test/kotlin/com/abrangencia/controller/PostalCodeControllerTest.kt
+++ abrangencia/src/test/kotlin/com/abrangencia/controller/PostalCodeControllerTest.kt
@@ -1,16 +1,23 @@
 package com.abrangencia.controller
 
+import com.example.model.PostalCode
+import com.example.model.Range
+import com.example.model.ShippingCompany
 import com.example.service.DefaultPostalCodeService
 import com.example.service.PostalCodeService
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
+import org.bson.types.ObjectId
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.mock
+import org.mockito.ArgumentMatchers
+import org.mockito.kotlin.*
 
 // Inicializa o micronaut, tbm inicializa o service
 @MicronautTest
@@ -31,10 +38,25 @@ class PostalCodeControllerTest {
         return mock()
     }
 
+    private val existentPostalCode = PostalCode(
+        postalCode = "09280650",
+        city = "Santo André",
+        state = "São Paulo",
+        codeType = "Batata"
+    )
+
     // Usei o exchange recebendo só uma string pois é uma verificação, não precisando de uma segunda, como um body
     @Test
-    fun `test get postalCode`() {
-        val response = client.toBlocking().exchange<String>("http://localhost:8080/postalcode/09280650")
-        assertEquals(HttpStatus.OK, response.status)
+    fun `test getPostalCode`() {
+        //given
+        whenever(service.getPostalCode(ArgumentMatchers.anyString())) doReturn existentPostalCode
+
+        //when
+        val response =
+            client.toBlocking().exchange<String, String>(HttpRequest.GET("/postalcode/09280650"))
+
+        //then
+        Assertions.assertEquals(HttpStatus.OK, response.status)
+        verify(service, times(1)).getPostalCode("09280650")
     }
 }
